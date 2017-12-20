7ab2b3cb882fc10750039acb319b09e8b15d597d
lockpic <a0920119383@gmail.com>
2017-12-20 15:24:22 +0800

    End of ch.9 half

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -4,5 +4,7 @@ from article import views
 
 urlpatterns = [
     url(r'^$', views.article, name='article'),
-    
+    url(r'^articleCreate/$', views.articleCreate, name='articleCreate'),
+    url(r'^articleRead/(?P<articleId>[0-9]+)/$', views.articleRead, name='articleRead'),
+
 ]

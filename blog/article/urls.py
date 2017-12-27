1dd6f4ce0cd86f58b31a594e43547447121801a8
lockpic <a0920119383@gmail.com>
2017-12-27 15:18:26 +0800

    ch9 End!!!!!!!!!

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^articleRead/(?P<articleId>[0-9]+)/$', views.articleRead, name='articleRead'),
     url(r'^articleUpdate/(?P<articleId>[0-9]+)/$', views.articleUpdate, name='articleUpdate'),
     url(r'^articleDelete/(?P<articleId>[0-9]+)/$', views.articleDelete, name='articleDelete'),
-     
+    url(r'^articleSearch/$', views.articleSearch, name='articleSearch'),
+        
 ]
